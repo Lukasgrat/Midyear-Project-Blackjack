1dac81f222f3eb19f84ee298ff2df1555760a9ce
Lukasgrat <60186475+Lukasgrat@users.noreply.github.com>
2020-01-28 11:10:56 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,1 +1,12 @@
 from tkinter import *
+class Application(Frame):
+    def __init__(self,master):
+        super().__init__(master)
+        self.grid()
+        self.create_widgets()
+    def create_widgets(self):
+      Label(text = "Welcome to Blackjack!",fg = "green").grid(row = 0,column = 1,sticky = N)
+root = Tk()
+root.title("BlackJack")
+app = Application(root)
+root.mainloop()
